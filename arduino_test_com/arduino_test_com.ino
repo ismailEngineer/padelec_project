af98feba60b0c74aec55740480ac282efee5c547
Ismail Hamrouni <ismail_hamrouni@yahoo.fr>
2025-12-27 00:23:48 +0100

    update script uno to send msg sent from stm32

--- arduino_test_com/arduino_test_com.ino
+++ arduino_test_com/arduino_test_com.ino
@@ -1,20 +1,31 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial stmSerial(2, 3); // RX, TX
+// RX, TX côté Arduino
+SoftwareSerial stm32Serial(8, 9);  // RX=D8, TX=D9
 
 void setup() {
-  Serial.begin(115200);       // PC
-  stmSerial.begin(115200);    // STM32
+  Serial.begin(115200);       // USB <-> PC
+  stm32Serial.begin(57600);   // UART <-> STM32
+
+  Serial.println("Arduino ready - USB <-> STM32 bridge");
 }
 
 void loop() {
-  // PC → STM32
+  // ---------- PC -> STM32 ----------
   if (Serial.available()) {
-    stmSerial.write(Serial.read());
+    char c = Serial.read();
+    stm32Serial.write(c);
   }
 
-  // STM32 → PC
-  if (stmSerial.available()) {
-    Serial.write(stmSerial.read());
+  // ---------- STM32 -> Arduino ----------
+  if (stm32Serial.available()) {
+    char c = stm32Serial.read();
+
+    // Affichage sur le Serial Monitor
+    Serial.print("STM32 -> ");
+    Serial.println(c);
+
+    // (optionnel) renvoyer au PC
+    Serial.write(c);
   }
 }
